36780cc1f2612f61619ff8af61c19b0b2820db67
Kendal Deihl <93454607+Kjdjr18@users.noreply.github.com>
2023-02-01 18:12:55 -0600

    added VPC

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,21 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = var.default_tags
+  }
+}
+
+# create VPC; CIDR 10.0.0.0/16
+resource "aws_vpc" "main" {
+  cidr_block                       = var.vpc_cidr
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
+  tags = {
+    "Name" = "${var.default_tags.env}-VPC"
+  }
+}
