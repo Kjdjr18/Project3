51d2f14c3db1f3fafcf2c41d1a1ab42fb0df97e6
Kendal Deihl <93454607+Kjdjr18@users.noreply.github.com>
2023-02-01 18:37:33 -0600

    added public and private subnets

--- vpc.tf
+++ vpc.tf
@@ -24,3 +24,27 @@ resource "aws_vpc" "main" {
     "Name" = "${var.default_tags.env}-VPC"
   }
 }
+
+# public subnets 10.0.0.0/24
+resource "aws_subnet" "public" {
+  count                   = var.public_subnet_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "${var.default_tags.env}-PublicSubnet-${data.aws_availability_zones.availability_zone.names[count.index]}"
+  }
+  availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
+}
+
+# private subnets
+resource "aws_subnet" "private" {
+  count      = var.private_subnet_count
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + var.public_subnet_count)
+  tags = {
+    "Name" = "${var.default_tags.env}-PrivateSubnet-${data.aws_availability_zones.availability_zone.names[count.index]}"
+  }
+  availability_zone = data.aws_availability_zones.availability_zone.names[count.index]
+}
+ 
